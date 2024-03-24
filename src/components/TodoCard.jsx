bf8f9bb3af60a5f43c281771000379235c31d5c4
Wesley Franks <code.wesleyfranks@gmail.com>
2024-03-24 18:28:30 -0500

    fixed css bug. Children for todoCard in wrong dive

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -4,18 +4,20 @@ export default function TodoCard(props) {
   const {children, handleDeleteTodo, index, handleEditTodo} = props
   return (
     <li className='todoItem' >
+      {children}
         <div className='actionsContainer'>
-            {children}
-            <button onClick={() => {
-              handleEditTodo(index)
-            }}>
-              <i className="fa-solid fa-pen-to-square"></i>
-            </button>
-            <button onClick={() => {
-              handleDeleteTodo(index)
-            }}>
-              <i className="fa-regular fa-trash-can"></i>
-            </button>
+            <div className='actionsButtons'>
+              <button onClick={() => {
+                handleEditTodo(index)
+              }}>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button onClick={() => {
+                handleDeleteTodo(index)
+              }}>
+                <i className="fa-regular fa-trash-can"></i>
+              </button>
+            </div>
         </div>
     </li>
   )
