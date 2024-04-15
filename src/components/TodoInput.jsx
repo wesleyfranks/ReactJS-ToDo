d49d7626b221c653b70cb24acdedb54443121f5a
Wesley Franks <code.wesleyfranks@gmail.com>
2024-04-14 23:14:28 -0500

    added onKeyDown function. tested. works.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,10 +2,25 @@ import { useState } from "react"
 
 export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue } = props
+
+    const addTodos = () => {
+        handleAddTodos(todoValue)
+        setTodoValue('')
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          addTodos()
+        }
+      };
+
     return (
         <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
+            <input 
+                value={todoValue} 
+                onKeyDown={handleKeyDown}
+                onChange={(e) => {
+                    setTodoValue(e.target.value)
             }} placeholder='Enter a todo...'/>
             <button onClick={() => {
                 handleAddTodos(todoValue)
